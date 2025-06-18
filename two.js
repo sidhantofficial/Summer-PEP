3fdf1d30913345e34ecd6d4050facf3a8d90d61d
sidhantofficial <119691987+sidhantofficial@users.noreply.github.com>
2025-06-19 01:05:23 +0530

    Update two.js

--- two.js
+++ two.js
@@ -1,45 +1,45 @@
-// // SCOPES OF VAR, CONST, LET
+// SCOPES OF VAR, CONST, LET
 
-// {
-//     let x = 10;
-//     const y = 11;
-//     var z = 110;
-// }
-// // console.log(x); // THIS THROWS AN ERROR X IS NOT DEFINED
-// // console.log(y); // THIS THROWS AN ERROR Y IS NOT DEFINED
-// // console.log(z); // THIS RUNS FINE
+{
+    let x = 10;
+    const y = 11;
+    var z = 110;
+}
+// console.log(x); // THIS THROWS AN ERROR X IS NOT DEFINED
+// console.log(y); // THIS THROWS AN ERROR Y IS NOT DEFINED
+// console.log(z); // THIS RUNS FINE
 
-// function any(){
-//     var variable = 34;
-// }
-// // console.log(variable); // THROWS AN ERROR VARIABLE IS NOT DEFINED
+function any(){
+    var variable = 34;
+}
+// console.log(variable); // THROWS AN ERROR VARIABLE IS NOT DEFINED
 
-// // SO THIS MEANS THAT LET AND CONST HAVE BLOCK SCOPE
-// // VAR HAS FUNCTIONAL SCOPE
+// SO THIS MEANS THAT LET AND CONST HAVE BLOCK SCOPE
+// VAR HAS FUNCTIONAL SCOPE
 
-// // WAYS TO MAKE A FUNCTION
+// WAYS TO MAKE A FUNCTION
 
-// myfun()
-// function myfun(){ // ONLY THIS TYPE OF FUNCTION SUPPORTS HOISTING
-//     console.log("executing function myfun");
-// }
+myfun()
+function myfun(){ // ONLY THIS TYPE OF FUNCTION SUPPORTS HOISTING
+    console.log("executing function myfun");
+}
 
-// myfunc(); // THROWS AN ERROR CANNOT ACCESS 'myfunc' BEFORE INITIALIZATION
-// const myfunc = function(){
-//     console.log("executing function myfunc");
+myfunc(); // THROWS AN ERROR CANNOT ACCESS 'myfunc' BEFORE INITIALIZATION
+const myfunc = function(){
+    console.log("executing function myfunc");
 
-// }
+}
 
-// myFunc(); // THROWS AN ERROR CANNOT ACCESS 'myfunc' BEFORE INITIALIZATION
-// const myFunc = () => {  
-//     console.log("executing function myFunc");
-// }
+myFunc(); // THROWS AN ERROR CANNOT ACCESS 'myfunc' BEFORE INITIALIZATION
+const myFunc = () => {  
+    console.log("executing function myFunc");
+}
 
-// // console.log(new_x); // CANNOT ACCESS NEW_X BEFORE INTIALIZATION
-// // let new_x = 10;
+// console.log(new_x); // CANNOT ACCESS NEW_X BEFORE INTIALIZATION
+// let new_x = 10;
 
-// console.log(new_y); // UNDEFINED
-// var new_y = 10;
+console.log(new_y); // UNDEFINED
+var new_y = 10;
 
 // OPERATIONS ON ARRAY
 
